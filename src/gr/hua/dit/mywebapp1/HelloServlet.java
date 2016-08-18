7b128bba142584f886b8981616f1eab889372e58
Anargyros Tsadimas <tsadimas@gmail.com>
2016-08-18 16:51:12 +0300

    alternative way to define servlet mapping without web.xml

--- src/gr/hua/dit/mywebapp1/HelloServlet.java
+++ src/gr/hua/dit/mywebapp1/HelloServlet.java
@@ -3,10 +3,17 @@ package gr.hua.dit.mywebapp1;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet(
+	       name = "helloServlet",
+	       urlPatterns = {"/greeting", "/salutation", "/wazzup"},
+	       loadOnStartup = 1
+	)
 public class HelloServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
